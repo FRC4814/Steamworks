ba24ab835e9fb06de10ca3f9114dd62af62c29af
Team 4814 <team4814@gmail.com>
2017-02-19 15:16:28 -0500

    Encoder tweaking

--- src/com/team4814/frc2017/subsystems/DriveTrain.java
+++ src/com/team4814/frc2017/subsystems/DriveTrain.java
@@ -2,6 +2,7 @@ package com.team4814.frc2017.subsystems;
 
 import com.team4814.frc2017.RobotMap;
 import com.team4814.frc2017.commands.HaloDriveCommand;
+import com.team4814.frc2017.utils.DashboardVariable;
 
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.Encoder;
@@ -21,10 +22,13 @@ public class DriveTrain extends Subsystem
 
 	public Encoder leftEncoder;
 	public Encoder rightEncoder;
+	
+	public static DashboardVariable<Double> leftDPP;
+	public static DashboardVariable<Double> rightDPP;
+	
 
 	public DriveTrain()
-	{
-
+	{		
 		leftMotors = new Victor[RobotMap.LEFT_MOTORS.length];
 		for (int i = 0; i < leftMotors.length; i++)
 		{
@@ -45,15 +49,18 @@ public class DriveTrain extends Subsystem
 
 		leftEncoder.setMaxPeriod(.1);
 		leftEncoder.setMinRate(10);
-		leftEncoder.setDistancePerPulse(5);
-		leftEncoder.setReverseDirection(false);
-		leftEncoder.setSamplesToAverage(7);
+		leftEncoder.setDistancePerPulse(0.192); // cm/pulse => 20 pulses/rev x 8.75 gear on a 4" dia wheel
+		leftEncoder.setReverseDirection(true);
+		leftEncoder.setSamplesToAverage(10);
 
 		rightEncoder.setMaxPeriod(.1);
 		rightEncoder.setMinRate(10);
-		rightEncoder.setDistancePerPulse(5);
+		rightEncoder.setDistancePerPulse(0.192); // cm/pulse => 20 pulses/rev x 8.75 gear on a 4" dia wheel
 		rightEncoder.setReverseDirection(false);
-		rightEncoder.setSamplesToAverage(7);
+		rightEncoder.setSamplesToAverage(10);
+		
+		//leftEncoder.setDistancePerPulse(leftDPP.get());
+		//rightEncoder.setDistancePerPulse(rightDPP.get());
 
 		LiveWindow.addSensor(this.getName(), "Left Encoder", leftEncoder);
 		LiveWindow.addSensor(this.getName(), "Right Encoder", rightEncoder);
@@ -72,19 +79,25 @@ public class DriveTrain extends Subsystem
 
 	public void drive(double leftSpeed, double rightSpeed)
 	{
-		double rightDistance = 0;
-		double leftDistance = 0;
+		if (leftDPP == null)
+		leftDPP = new DashboardVariable<Double>("LeftDPP", 0.197);
+		if (rightDPP == null)
+		rightDPP = new DashboardVariable<Double>("RightDPP", 0.192);
+		
+		leftEncoder.setDistancePerPulse(leftDPP.get());
+		rightEncoder.setDistancePerPulse(rightDPP.get());
+		
 		SmartDashboard.putNumber("Left Encoder", leftEncoder.get());
 		SmartDashboard.putNumber("Right Encoder", rightEncoder.get());
+		
+		SmartDashboard.putNumber("Left Encoder D", leftEncoder.getDistance());
+		SmartDashboard.putNumber("Right Encoder D", rightEncoder.getDistance());
 
 		setLeftSpeed(leftSpeed);
 		setRightSpeed(rightSpeed);
-
-		rightDistance = getRightDistnace();
-		leftDistance = getLeftDistnace();
 	}
 
-	public void setLeftSpeed(double speed)
+	protected void setLeftSpeed(double speed)
 	{
 		for (int i = 0; i < leftMotors.length; i++)
 			leftMotors[i].set(speed);
@@ -92,7 +105,7 @@ public class DriveTrain extends Subsystem
 		SmartDashboard.putNumber("Drive Left Speed", speed);
 	}
 
-	public void setRightSpeed(double speed)
+	protected void setRightSpeed(double speed)
 	{
 		for (int i = 0; i < rightMotors.length; i++)
 			rightMotors[i].set(speed);
@@ -100,14 +113,15 @@ public class DriveTrain extends Subsystem
 		SmartDashboard.putNumber("Drive Right Speed", speed);
 	}
 
-	public double getLeftDistnace()
+	public double getLeftDistance()
 	{
 		return leftEncoder.getDistance();
 	}
 
-	public double getRightDistnace()
+	public double getRightDistance()
 	{
 		return rightEncoder.getDistance();
 	}
+
 
 }
